6ff05f2133e777a46d852571a95837fa5f18d753
Will Pearse <will.pearse@gmail.com>
2018-11-30 10:29:18 -0700

    Better tests; new contributor! :D

--- tests/test-all.R
+++ tests/test-all.R
@@ -7,12 +7,12 @@ test_that("Weibull estimates are sensible",{
     expect_equal(
         weib.limit(c(1662, 1638, 1631, 1628, 1628, 1611, 1607, 1602, 1601, 1598), upper=TRUE),
         setNames(c(1690, 1799, 1669), c("estimate","conf-interval","conf-interval")),
-        tolerance=.1
+        tolerance=0.0005
     )
     expect_equal(
         weib.limit(c(1662, 1638, 1631, 1628, 1628, 1611, 1607, 1602, 1601, 1598)),
         setNames(c(1595, 1575, 1597), c("estimate","conf-interval","conf-interval")),
-        tolerance=.1
+        tolerance=0.0005
     )
     
 })
